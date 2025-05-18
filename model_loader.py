a946a8a2d14750507d201b2f5befc52b5eeb1dfc
HexLang-OFFICIAL <cj.hauser@mail.com>
2025-05-18 03:56:57 +0000

    delete fils start from scratch

--- model_loader.py
+++
@@ -1,9 +0,0 @@
-from transformers import AutoModelForCausalLM, AutoTokenizer
-
-MODEL_NAME = "tiiuae/falcon-rw-1b"
-
-def load_model():
-    print("Loading model...")
-    tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME)
-    model = AutoModelForCausalLM.from_pretrained(MODEL_NAME)
-    return model, tokenizer
